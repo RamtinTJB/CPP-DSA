08a450dd92946054ae9fda35e773a2130c3c184d
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-12-28 20:55:05 -0800

    Add quicksort

---
+++ Sort/benchmark.cpp
@@ -0,0 +1,9 @@
+#include "sort.h"
+
+int main() {
+    std::vector<int> vec {1, 2, 10, 5};
+
+    quicksort(vec);
+
+    return 0;
+}
---
+++ Sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef CPP_DSA_SORT_H
+#define CPP_DSA_SORT_H
+
+#include <vector>
+
+void quicksort(std::vector<int>&);
+void heapsort(std::vector<int>&); // TODO
+void mergesort(std::vector<int>&); // TODO
+void introsort(std::vector<int>&); // TODO
+
+#endif
